ee98cb4f91168fd6c3cc38b0d66c309ac2ec6549
Mishator <voyteh89@mail.ru>
2023-05-06 16:19:28 +0300

    Все объекты реализованы в Main.

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        Car car2 = new Car();
-        car.setModelName("car1");
-        car2.setModelName("car2");
-        car.setWheelsCount(4);
-        car2.setWheelsCount(4);
+        Car car = new Car("car1");
+        Car car2 = new Car("car2");
 
-        Truck truck = new Truck();
-        Truck truck2 = new Truck();
-        truck.setModelName("truck1");
-        truck2.setModelName("truck2");
-        truck.setWheelsCount(6);
-        truck2.setWheelsCount(8);
+        Truck truck = new Truck("truck1");
+        Truck truck2 = new Truck("truck2", 8);
 
-        Bicycle bicycle = new Bicycle();
-        Bicycle bicycle2 = new Bicycle();
-        bicycle.setModelName("bicycle1");
-        bicycle2.setModelName("bicycle2");
-        bicycle.setWheelsCount(2);
-        bicycle2.setWheelsCount(2);
+        Bicycle bicycle = new Bicycle("bicycle1");
+        Bicycle bicycle2 = new Bicycle("bicycle2");
 
+        Transport[] transports = {
+                (Transport) car,
+                (Transport) car2,
+                (Transport) truck,
+                (Transport) truck2,
+                (Transport) bicycle,
+                (Transport) bicycle2
+        };
         ServiceStation station = new ServiceStation();
-        station.check(car, null, null);
-        station.check(car2, null, null);
-        station.check(null, bicycle, null);
-        station.check(null, bicycle2, null);
-        station.check(null, null, truck);
-        station.check(null, null, truck2);
+        for (Transport t : transports) {
+            station.check(t);
+        }
     }
+
 }
